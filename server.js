b7c6497262398eea3f5f9417d2d9e91290387a7c
DUC-HA <phucducha12@gmail.com>
2025-09-09 11:56:02 +0700

    Add listen port Render

--- server.js
+++ server.js
@@ -196,3 +196,7 @@ server.listen(3000, () => {
     console.log("🚀 Server chạy tại http://localhost:3000");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
